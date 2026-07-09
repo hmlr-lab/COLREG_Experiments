d7dc58f54a0be9828f5ecae530e98cadc6eb7a9a
danyvarghese <gagiprha@gmail.com>
2026-07-09 12:58:34 +0100

    .

--- Learning.py
+++ Learning.py
@@ -25,6 +25,8 @@ file_path_log = ["260630_examples/log_0.txt",
 # Generate stage 1 BK files from log files
 facts, examples =  generate_bk_from_log(file_path_log, verbose=False)
 
+# Save the examples
+write_prolog_file(examples, "examples.pl")
 # Save the generated Prolog knowledge base (facts and rules) for evaluation.
 write_prolog_file(facts,"facts.pl")
 
