01ad5048b2cb782587ab2217a69f82e10e60cd3d
JamesTrewern <trewern.james@gmail.com>
2026-07-09 14:44:54 +0100

    Init commit

---
+++ build_prolog.py
@@ -0,0 +1,17 @@
+from util import *
+
+# Example log files
+file_path_log = ["260630_examples/log_0.txt",
+                 "260630_examples/log_1.txt",
+                "260630_examples/log_2.txt",
+                "260630_examples/log_3.txt",
+                "260630_examples/log_4.txt",
+                 ]
+
+# Generate stage 1 BK files from log files
+facts, examples =  generate_bk_from_log(file_path_log, verbose=False)
+
+# Save the examples
+write_prolog_file(examples, "examples.pl")
+# Save the generated Prolog knowledge base (facts and rules) for evaluation.
+write_prolog_file(facts,"facts.pl")
