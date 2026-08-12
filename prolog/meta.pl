9b3df9fcc5e62be3e2ae1b13089158909db15bc7
James Trewern <trewern.james@gmail.com>
2026-08-12 15:57:08 +0100

    Force transitive properties for tcpa, dcpa, range

--- prolog/meta.pl
+++ prolog/meta.pl
@@ -5,7 +5,6 @@
 comparitor(X):-
     member(X, [less_than,less_or_equal, greater_than, greater_or_equal]).
 % % Instantiate self
-% X(X),{X}. 
 % Single Condition
 P(X):-
     comparitor(Q),
@@ -72,8 +71,45 @@ P(X,Y):-
     R(X,Y),
     {P,Q,R,A,B}.
 
-insubstantial(insubstantial).
-small(small).
-moderate(moderate).
-large(large).
-very_large(very_large).
+% Force transitive for tcpa - dcpa - range
+% Single Comparison
+P(X,Y):-
+    tcpa(X,Y,Z),
+    comparitor(Q),
+    Q(Z,A),
+    [P,Q,A].
+P(X,Y):-
+    dcpa(X,Y,Z),
+    comparitor(Q).
+    Q(Z,A).
+    [P,Q,A].
+P(X,Y):-
+    range(X,Y,Z),
+    comparitor(Q),
+    Q(Z,A),
+    [P,Q,A].
+% Double comparison
+P(X,Y):-
+    tcpa(X,Y,Z),
+    comparitor(Q),
+    comparitor(R),
+    Q(Z,A),
+    R(Z,B),
+    {Q,R}
+    [P,A,B].
+P(X,Y):-
+    dcpa(X,Y,Z),
+    comparitor(Q),
+    comparitor(R),
+    Q(Z,A),
+    R(Z,B),
+    {Q,R}
+    [P,A,B].
+P(X,Y):-
+    range(X,Y,Z),
+    comparitor(Q),
+    comparitor(R),
+    Q(Z,A),
+    R(Z,B),
+    {Q,R}
+    [P,A,B].
