b2949a9d3c24ae4dac9381fea423054ebab457b3
James Trewern <trewern.james@gmail.com>
2026-09-02 15:53:55 +0100

    test run

--- prolog2-runner/src/main.rs
+++ prolog2-runner/src/main.rs
@@ -10,17 +10,27 @@ fn main() {
     let situations = Situation::load_situations();
     generate_prolog(situations);
 
-    iterate_max_clause_pred("setup_take_action.json", 2, 4);
+    // iterate_max_clause_pred("setup_take_action.json", 2, 4);
+    
+    let app = App::from_setup_json("setup.json").unwrap().config(Config {
+        max_depth: 5,
+        max_clause: 2,
+        max_pred: 1,
+        debug: false,
+    });
+    app.run();
 }
 
-fn iterate_max_clause_pred(setup_path: &str, max_max_pred: usize, max_max_clause: usize) {
-    for max_clause in 1..max_max_clause+1 {
-        for max_pred in 0..max_max_pred+1 {
+fn iterate_max_clause_pred(setup_path: &str, mut max_max_pred: usize, mut max_max_clause: usize) {
+    max_max_pred += 1;
+    max_max_clause += 1;
+    for max_clause in 1..max_max_clause {
+        for max_pred in 1..max_max_pred {
             println!("=================================");
             println!("Try max_clause: {max_clause}, max_pred: {max_pred}");
             println!("=================================");
             let app = App::from_setup_json(setup_path).unwrap().config(Config {
-                max_depth: 20,
+                max_depth: 5,
                 max_clause,
                 max_pred,
                 debug: false,
